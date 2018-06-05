0fd87d1f727d4808c796636edd1ba464bc0e774d
Joseph E <jesquitin@att.net>
2018-06-05 17:11:03 -0500

    updated routes to start app port to listen on 8080

---
+++ routes/api-routes.js
@@ -0,0 +1,13 @@
+// *********************************************************************************
+// api-routes.js - this file offers a set of routes for displaying and saving data to the db
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+
+// Requiring our  model
+var db = require("../models");
+
+module.exports = function(app){
+
+};
---
+++ routes/html-routes.js
@@ -0,0 +1,10 @@
+// html-routes.js - this file offers a set of routes for sending users to the various html pages
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+var path = require("path");
+
+module.exports = function(app){
+
+};
