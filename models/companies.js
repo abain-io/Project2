bc2111fdc939f028c1ea43e58b656241c0abf1ac
Isha Khan <khan.isha08@gmail.com>
2018-06-14 10:26:52 -0500

    responsive

--- models/companies.js
+++ models/companies.js
@@ -1,52 +1,52 @@
 // companies.js
-module.exports = function(sequelize, DataTypes) {
-    var Company = sequelize.define("Company", {
-      // ID will be provided automatically by Sequelize
-      // Giving the Company model a name of type STRING
-      co_name: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      co_url: {
-        type: DataTypes.STRING
-      },
-      co_email: {
-        type: DataTypes.STRING
-      },
-      co_phone: {
-        type: DataTypes.STRING
-        
-      },
-      co_address: {
-        type: DataTypes.STRING
-      },
-      co_city: {
-        type: DataTypes.STRING
-      },
-      co_state: {
-        type: DataTypes.STRING
-      },
-      priority: {
-        type: DataTypes.STRING
-      }
+module.exports = function (sequelize, DataTypes) {
+  var Company = sequelize.define("Company", {
+    // ID will be provided automatically by Sequelize
+    // Giving the Company model a name of type STRING
+    co_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    co_url: {
+      type: DataTypes.STRING
+    },
+    co_email: {
+      type: DataTypes.STRING
+    },
+    co_phone: {
+      type: DataTypes.STRING
+
+    },
+    co_address: {
+      type: DataTypes.STRING
+    },
+    co_city: {
+      type: DataTypes.STRING
+    },
+    co_state: {
+      type: DataTypes.STRING
+    },
+    priority: {
+      type: DataTypes.STRING
+    }
 
 
-    });
-    // The following is how we associated (joined) Author to Posts in blogger app, just for our info
-    // We will be creating a companyContact model and then using something like:
+  });
+  // The following is how we associated (joined) Author to Posts in blogger app, just for our info
+  // We will be creating a companyContact model and then using something like:
 
-    // Company.belongsToMany(Contact, {through: CompanyContact });
-    // Contact.belongsToMany(Company, {through: CompanyContact }); 
-    
-    // Author.associate = function(models) {
-    // Associating Author with Posts
-    // When an Author is deleted, also delete any associated Posts
-    // Author.hasMany(models.Post, {
-    // onDelete: "cascade"
-    //   });
-    // };
-    // Company.associate = function(models) {Company.belongsToMany(models.Contact, { as: 'Contact', through: { model: CompanyContact, unique: false }, foreignKey: 'contactId' });}
-    
+  // Company.belongsToMany(Contact, {through: CompanyContact });
+  // Contact.belongsToMany(Company, {through: CompanyContact }); 
 
-    return Company;
-  }; 
+  // Author.associate = function(models) {
+  // Associating Author with Posts
+  // When an Author is deleted, also delete any associated Posts
+  // Author.hasMany(models.Post, {
+  // onDelete: "cascade"
+  //   });
+  // };
+  // Company.associate = function(models) {Company.belongsToMany(models.Contact, { as: 'Contact', through: { model: CompanyContact, unique: false }, foreignKey: 'contactId' });}
+
+
+  return Company;
+}; 
--- server.js
+++ server.js
@@ -171,8 +171,8 @@ app.use(routes);
 
 
 // Syncing our database and logging a message to the user upon success
-db.sequelize.sync().then(function() {
-  app.listen(PORT, function() {
+db.sequelize.sync().then(function () {
+  app.listen(PORT, function () {
     console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
   });
 });
